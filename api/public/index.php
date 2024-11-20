af2ea988bf9f36bb2d39d50c07e42cb4faa3c834
Jannis Westphal <jannis.westphal@gmail.com>
2024-11-20 14:34:01 +0100

    add namespace

--- api/public/index.php
+++ api/public/index.php
@@ -1,11 +1,13 @@
 <?php
+use App\Core\Router;
 
 const BASE_PATH = __DIR__ . "/../";
 
 require str_replace('/', DIRECTORY_SEPARATOR, BASE_PATH  . 'App/Core/functions.php');
 
 spl_autoload_register(function($class){
-    require base_path("App/Core/$class.php");
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    require base_path("$class.php");
 });
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
