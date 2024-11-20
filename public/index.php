d25d955bb6af1fa00f8d312f14c943463c6fce70
Jannis Westphal <jannis.westphal@gmail.com>
2024-11-20 10:57:36 +0100

    testing stuff out

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
 
 echo 'meow';
-?>
+
+const BASE_PATH = __DIR__ . "/../";
+
+require str_replace('/', DIRECTORY_SEPARATOR, BASE_PATH  . 'App/Core/functions.php');
+
+//echo base_path('test/test.php');
+
+// Extracts the URL path without the query string
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+dd($path);
+dd($method);
